bbd31877d085b28be292bf71976f36a1f05b10c1
John Liu <stoic.llama@gmail.com>
2023-11-10 14:29:25 -0500

    Change to static

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-
 require('dotenv').config()
 
 const express = require('express')
+const path = require('path');
 const app = express()
 
-app.use(express.static('public'))
-
 const port = process.env.PORT || 9999
+
+app.use(express.static('public'))
 
 app.listen(port, () => {
   console.log(`Capstone Metrics Dashboard listening on port ${port}`)
