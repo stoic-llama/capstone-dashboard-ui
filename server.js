e51d7d30c38611f4d14972fa8f42834db797847c
John Liu <stoic.llama@gmail.com>
2023-11-10 14:37:53 -0500

    Added healthcheck

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const port = process.env.PORT || 9999
 
 app.use(express.static('public'))
 
+app.get('/healthcheck', (req, res) => {
+  res.send(`API is alive on ${port}!`);
+});
+
 app.listen(port, () => {
   console.log(`Capstone Metrics Dashboard listening on port ${port}`)
 })
